c70d7189db8600c2d3c82567978f9ffff101604e
MuhammadHammad-hub <k256501@nu.edu.pk>
2026-02-07 08:18:55 -0800

    Added variables and data types program

--- intro.c
+++ intro.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Name:\tMuhammad Hammad\n");
-    printf("Roll Number:\t25K-6501\n");
-    printf("Section:\tA\n");
+    int a = 10;
+    float b = 3.5;
+    double c = 9.87654;
+    char d = 'H';
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %f\n", b);
+    printf("Double value: %lf\n", c);
+    printf("Character value: %c\n", d);
+
     return 0;
 }
