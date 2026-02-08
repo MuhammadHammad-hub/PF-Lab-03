994948a5300077016c5750326329fdb41638971c
MuhammadHammad-hub <k256501@nu.edu.pk>
2026-02-08 21:53:47 +0500

    PF-LAB-03 TASK UPLOADED

---
+++ T6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name:\tMuhammad Hammad\n");
+    printf("Roll Number:\t25K-6501\n");
+    printf("Section:\tA\n");
+    return 0;
+}
---
+++ T7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 3.5;
+    double c = 9.87654;
+    char d = 'H';
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %f\n", b);
+    printf("Double value: %lf\n", c);
+    printf("Character value: %c\n", d);
+
+    return 0;
+}
